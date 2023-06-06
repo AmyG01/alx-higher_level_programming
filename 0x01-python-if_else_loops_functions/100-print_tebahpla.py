3bb8059988208066330e85767919f1597a7dcdff
Gugu Mtonjeni <gammtonjeni@gmail.com>
2023-06-06 16:42:11 -0700

    corrections

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print(chr(i).lower() if i % 2 == 0 else chr(i).upper(), end=' ')
+for letter in range(122, 96, -1):
+    if letter % 2 != 0:
+        letter -= 32
+    print("{}".format(chr(letter)), end="")
