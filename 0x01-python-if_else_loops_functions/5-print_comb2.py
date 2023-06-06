7b584685e98186c1d7df85ecdb3022c35a3a02d1
Gugu Mtonjeni <gammtonjeni@gmail.com>
2023-06-06 16:28:39 -0700

    corrections

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    print(chr(i), end='')
-    if i % 2 == 0:
-        i -= 1
+for i in range(0, 100):
+    if i < 99:
+        print("{:02d}".format(i), end=", ")
     else:
-        i += 1
+        print("{:02d}".format(i))
