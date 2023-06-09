c6f7fadd171661abb3270b0405b1b111279a4def
Gugu Mtonjeni <gammtonjeni@gmail.com>
2023-06-09 13:09:13 -0700

    Import modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import hidden_4
-    for i in dir(hidden_4):
-        if i[:2] != "__":
-            print("{:i}".format(i))
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
